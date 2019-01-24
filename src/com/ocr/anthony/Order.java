6187be4d55e2f99c7ad22c67fdffc596d1c16198
Amal <amalsouheil@gmail.com>
2019-01-24 23:43:36 +0100

    L'ajout des exceptions

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -1,5 +1,6 @@
 package com.ocr.anthony;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Order {
@@ -170,12 +171,33 @@ public class Order {
     /**
      * Run asking process for several menus.
      */
-
     public void runMenus() {
 
         System.out.println("Combien souhaitez vous commander de menu ?");
 
-        int menuQuantity = sc.nextInt();
+        int menuQuantity = -1;
+
+        boolean responseIsGood;
+
+        do {
+
+            try {
+
+                menuQuantity = sc.nextInt();
+
+                responseIsGood = true;
+
+            } catch (InputMismatchException e) {
+
+                sc.next();
+
+                System.out.println("Vous devez saisir un nombre, correspondant au nombre de menus souhaités");
+
+                responseIsGood = false;
+
+            }
+
+        } while (!responseIsGood);
 
         orderSummary = "Résumé de votre commande :%n";
 
@@ -198,54 +220,41 @@ public class Order {
      * @param responses available responses
      * @return the number of the selected choice
      */
+    /**
+     * Display a question about a category in the standard input, get response and display it
+     * @param category the category of the question
+     * @param responses available responses
+     * @return the number of the selected choice
+     */
     public int askSomething(String category, String[] responses) {
-
         System.out.println("Choix " + category);
-
         for (int i = 1; i <= responses.length; i++)
-
             System.out.println(i + " - " + responses[i - 1]);
-
         System.out.println("Que souhaitez-vous comme " + category + "?");
-
-        int nbResponse;
-
+        int nbResponse = 0;
         boolean responseIsGood;
-
         do {
-
-            nbResponse = sc.nextInt();
-
-            responseIsGood = (nbResponse >= 1 && nbResponse <= responses.length);
-
-            if (responseIsGood) {
-
-                String choice = "Vous avez choisi comme " + category + " : " + responses[nbResponse - 1];
-
-                orderSummary += choice + "%n";
-
-                System.out.println(choice);
-
-            } else {
-
-                boolean isVowel = "aeiouy".contains(Character.toString(category.charAt(0)));
-
-                if (isVowel)
-
-                    System.out.println("Vous n'avez pas choisi d'" + category + " parmi les choix proposés");
-
-                else
-
-                    System.out.println("Vous n'avez pas choisi de " + category + " parmi les choix proposés");
-
+            try {
+                nbResponse = sc.nextInt();
+                responseIsGood = (nbResponse >= 1 && nbResponse <= responses.length);
+            } catch (InputMismatchException e) {
+                sc.next();
+                responseIsGood = false;
             }
-
+            if (responseIsGood) {
+                String choice = "Vous avez choisi comme " + category + " : " + responses[nbResponse - 1];
+                orderSummary += choice + "%n";
+                System.out.println(choice);
+            } else {
+                boolean isVowel = "aeiouy".contains(Character.toString(category.charAt(0)));
+                if (isVowel)
+                    System.out.println("Vous n'avez pas choisi d'" + category + " parmi les choix proposés");
+                else
+                    System.out.println("Vous n'avez pas choisi de " + category + " parmi les choix proposés");
+            }
         } while (!responseIsGood);
-
         return nbResponse;
-
     }
-
 
     /**
      * Display a question about menu in the standard input, get response and display it
